a1663a2429908d77f4834ce5a97dce67ec69a98c
Jonas Holbech <jh@jonasholbech.dk>
2020-11-30 22:55:31 +0100

    this is so cool

---
+++ functions/get-all-tasks.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+exports.handler = async function(event, context) {
+    const MongoClient = require('mongodb').MongoClient;
+    const uri = process.env.MONGO_ATLAS_KEY;
+    const client = await MongoClient.connect(uri, { useUnifiedTopology: true,useNewUrlParser: true });
+    const db = client.db("project-m-database");    
+    const all = await db.collection('tasks').find({}).toArray();
+    
+    /* await db.collection("tasks").insertOne({
+        title:"Do stuff",
+        dueAt: Date.now(),
+        addedAt:Date.now()-5000,
+        description:"Bla, bla",
+        assigned:[]
+     });*/
+    client.close();
+    return {
+        statusCode: 200,
+        body: JSON.stringify(all)
+    };
+}
