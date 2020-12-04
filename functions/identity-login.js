434f7fe56a44334036ec0c7754673f561b614506
Jonas Holbech <jh@jonasholbech.dk>
2020-12-04 23:59:23 +0100

    hmm

--- functions/identity-login.js
+++ functions/identity-login.js
@@ -6,10 +6,25 @@ export const handler = async (req, _context) => {
     if (eventType === 'login') {
       console.log(`User: ${user.id} logged in`)
     }
-  
+    const validateUser = email => { 
+        if (email.split("@")[1] === "kea.dk") { 
+            return ["teacher"]; 
+        } else { 
+            return ["visitor"]; 
+        }
+    }; 
+    const roles = validateUser(user.email); 
+    const responseBody = { 
+        app_metadata: { 
+            roles, 
+            my_user_info: "this is user info that the user can’t change from the UI" 
+        }, 
+        user_metadata: { ...user.user_metadata, custom_data_from_function: "hurray this is some extra metadata" } 
+    }; 
     return {
-      statusCode: 200,
-    }
+        statusCode: 200,
+        body: JSON.stringify(responseBody)
+    };
   }
   /*
 exports.handler = function(event, context) { 
