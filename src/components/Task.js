ce17bfafbd1fd833661c67c94dc889c23ce6bca4
Jonas Holbech <jh@jonasholbech.dk>
2020-12-18 08:43:43 +0100

    remove unused dispatch

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,20 +1,22 @@
-import {useContext} from "react";
+import { useContext } from "react";
 import { store } from "../models/store.js";
 //import netlifyIdentity from 'netlify-identity-widget';
 import AssigneeForm from "./AssigneeForm";
 
-export default function Task(props){
-    const { globalState,dispatch } = useContext(store);
-    //const user = netlifyIdentity.currentUser();
+export default function Task(props) {
+  const { globalState } = useContext(store);
+  //const user = netlifyIdentity.currentUser();
 
-    const thisId = props.match.params.id;
-    const thisTask = globalState.tasks.find(el=>el._id===thisId);
-    
-    return <div className="Task">
-        <p className="titleLabel">Task</p>
-        <h1>{thisTask.title}</h1>
-        <p>by {thisTask.createdBy}</p>
-        <section className="description">{thisTask.description}</section>
-        <AssigneeForm {...thisTask}/>
+  const thisId = props.match.params.id;
+  const thisTask = globalState.tasks.find((el) => el._id === thisId);
+
+  return (
+    <div className="Task">
+      <p className="titleLabel">Task</p>
+      <h1>{thisTask.title}</h1>
+      <p>by {thisTask.createdBy}</p>
+      <section className="description">{thisTask.description}</section>
+      <AssigneeForm {...thisTask} />
     </div>
+  );
 }
